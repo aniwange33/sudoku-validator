67fd2e89f08524d89d9fce8ffbc31738af800481
aniwange33 <terteseamos@gmail.com>
2022-10-01 10:50:54 +0100

    feat: project setup complete and ready for development

---
+++ src/main/java/org/luxoft/SudokuValidator.java
@@ -0,0 +1,13 @@
+package org.luxoft;
+
+/**
+ * Hello world!
+ *
+ */
+public class SudokuValidator
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
